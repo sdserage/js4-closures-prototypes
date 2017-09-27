08a2f8e23775750daaec87555fa7da7e10b2af77
Scott Serage <xttocsurge51@gmail.com>
2017-09-27 17:14:42 -0600

    Completed closures inner.

--- closures.js
+++ closures.js
@@ -8,188 +8,189 @@ function outer() {
       return 'The original name was ' + name;
     }
   }
-  
+
   /****** INSTRUCTIONS PROBLEM 1 ******/
   /* Above you're given a function that returns another function which has a
   closure over the name variable. Invoke outer saving the return value into
   another variable called 'inner'. */
-  
+
   // Code Here
-  
+let inner = outer();
+
   //Once you do that, invoke inner.
-  
+
   //Code Here
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+inner();
+
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-02
   \******************************************************************************/
-  
-  
+
+
   function callFriend(name) {
     function dial(number) {
       return 'Calling ' + name + ' at ' + number
     }
     return dial;
   }
-  
+
   /****** INSTRUCTIONS PROBLEM 2 ******/
   /* Above you're given a callFriend function that returns the dial function.
-  
+
   Store the result of invoking callFriend in a variable named callJake.
-  
-  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248' 
+
+  When callJake is invoked with '435-555-9248', it returns 'Calling Jake at 435-555-9248'
   (HINT: You will need to pass in arguments to both function invocations)
   */
-  
+
     //Code Here
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-03
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 3 ******/
   /* Write a function called makeCounter that makes the following code work
   properly. */
-  
+
   //Code Here
-  
+
   //Uncomment this once you make your function
   //   var count = makeCounter();
   //   count(); // 1
   //   count(); // 2
   //   count(); // 3
   //   count(); // 4
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-04
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 4 ******/
   /* Inside the function called counterFactory return two functions that implement
   up/down counter. The first function is called inc, this function is responsible
-  for incrementing the value once and returning the updated value. The second function 
-  is called dec, this function is responsible for decrementing the value by one 
-  and returning the updated value. You will need to use the module pattern to 
-  achieve this. Information on the module pattern available here: 
+  for incrementing the value once and returning the updated value. The second function
+  is called dec, this function is responsible for decrementing the value by one
+  and returning the updated value. You will need to use the module pattern to
+  achieve this. Information on the module pattern available here:
   http://stackoverflow.com/questions/17776940/javascript-module-pattern-with-example?answertab=votes#tab-top
   */
-  
+
   function counterFactory(value) {
-  
+
     // Code here.
-  
-  
+
+
     return {
 
     }
   }
-  
-  
+
+
   counter = counterFactory(10);
   // counter.inc() // 11
   // counter.inc() // 12
   // counter.inc() // 13
   // counter.dec() // 12
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-05
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 5 ******/
   /* Inside the motivation function create another function called message that
   will return the welcome text with the firstname and lastname. The final message should
-  say "You're doing awesome, keep it up firstname lastname." 
+  say "You're doing awesome, keep it up firstname lastname."
   (Hint: don't forget to have a space between the firstname and lastname and a period
   at the end of the sentence.)  */
-  
+
   function motivation(firstname, lastname) {
-  
+
     var welcomeText = 'You\'re doing awesome, keep it up ';
-  
+
     // code message function here.
-  
-  
+
+
     //Uncommment this to return the value of your message function
     //return message;
-  
+
   }
-  
+
   var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
-  
-  
-  
-  
-  
-  
-  
-  
-  
-  
+
+
+
+
+
+
+
+
+
+
   /******************************************************************************\
       #PROBLEM-06
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 6 ******/
   /* Inside the module's return object create a publicMethod function that
   invokes privateMethod (return the result). Invoke this by calling module.publicMethod(); outside
   the module scope */
-  
+
   var module = (function() {
     var person = {
       name: "phillip",
       age: 29,
       location: "Utah"
     };
-  
+
     function privateMethod(){
       return "Hi, I'm " + person.name + ", age " + person.age + " from " + person.location;
     }
-  
+
     // Anything that is being returned is made public and can be invoked from
     // outside our lexical scope
     return {
       // Code here.
     };
-  
+
   })();
-  
-  
-  
+
+
+
   /******************************************************************************\
    #PROBLEM-07
    \******************************************************************************/
@@ -205,13 +206,13 @@ function outer() {
       // Code here
     }
   }
-  
-  
-  
+
+
+
   /******************************************************************************\
       #PROBLEM-08
   \******************************************************************************/
-  
+
   /****** INSTRUCTIONS PROBLEM 8 ******/
   /* Here we have a for loop that will iterate as long as i is less than or equal
   to 5. What we need to do is console.log(i) so that it logs like so:
@@ -224,10 +225,10 @@ function outer() {
    However, because each call to console.log occurs after the loop has finished,
    the value of i has changed before the console.log executes. We'll need to use
    a closure to preserve a reference to i at the time of execution.
-  
+
    Fix the code below to log the desired output.
    */
-  
+
   function timeOutCounter() {
     for (var i = 0; i <= 5; i++) {
       setTimeout(function() {
@@ -236,4 +237,3 @@ function outer() {
     }
   }
   timeOutCounter();
-  
